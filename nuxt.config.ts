07475ea790d340a8704b380eb757d7e67ec7a948
ahmetcan <ahmetcanisik545@gmail.com>
2025-05-26 16:49:02 +0300

    DEPENDENCIES

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { searchForWorkspaceRoot } from 'vite'
+
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	modules: ["@nuxt/ui", "@nuxt/fonts", "motion-v/nuxt"],
@@ -15,6 +17,17 @@ export default defineNuxtConfig({
 				"warning",
 				"error",
 			],
+		},
+	},
+	vite: {
+		server: {
+			fs: {
+				allow: [
+					searchForWorkspaceRoot(process.cwd()),
+					'C:/Users/ahmet/node_modules/', // Allow access to the directory causing issues
+					'c:/Users/ahmet/Desktop/bitirme/frontend/node_modules/' // Also allow project's own node_modules
+				],
+			},
 		},
 	},
 });
